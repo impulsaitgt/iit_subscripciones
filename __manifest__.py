4e6514137d1bbbf3c5f53f0f68ee7b0ebf974a65
Alexander Paiz <impulsaitgt@gmail.com>
2023-01-27 16:42:07 -0600

    Primer commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Especializacion Subscripciones ',
+    'summary':"""
+        Especializacion Subscripciones
+    """,
+    'author':'Alexander Paiz Paiz',
+    'category': 'General',
+    'version' : '1.0.0',
+    'depends': [
+        "account"
+    ],
+    'data': [
+    ]
+}
